b00d0cc8415f933bff61cb412b9ba984355b539a
Mateus andrade <osproks852@gmail.com>
2024-03-26 14:24:24 -0300

    a

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,17 @@
 import nextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
-const id = "1039647629083-le869tcjlnbstu5rp15uv2gprbfcf9as.apps.googleusercontent.com"
-const  clientsecret ="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"
-const secret = "19277c4845adb7cab70dd2876a7f8d15"
+
 
 //http://localhost:3000/api/auth/callback/google
 export const authOptions = {
     
     providers:[
         GoogleProvider({
-            clientId:id as string,
-            clientSecret:clientsecret as string,
+            clientId:process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
         })
     ],
- secret: secret as string
+ secret: process.env.JWT_SECRET as string
 }
 
 export default nextAuth(authOptions)
